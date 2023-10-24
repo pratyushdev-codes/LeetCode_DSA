13237a3d6bb0e5390a149baafd0bb570992b52e3
Pratyush Birole <birole.pratyush@gmail.com>
2023-10-24 20:31:43 +0530

    Add files via upload

---
+++ Arrays/SortColors.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SortColors{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sortColors(arr);
+        System.out.println(arr);
+    }public static void sortColors(int arr[]) {
+        int n = arr.length; // Added this line to get the length of the array
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
