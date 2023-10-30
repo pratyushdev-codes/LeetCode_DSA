763a3df6227b386236cbb28e4299ae61d69788d0
Pratyush Birole <birole.pratyush@gmail.com>
2023-10-30 22:29:29 +0530

    Add files via upload

---
+++ Strings/ValidAnangram.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class ValidAnangram{
+    public static void main(String[] args) {
+        
+    }lass Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+}
+}
