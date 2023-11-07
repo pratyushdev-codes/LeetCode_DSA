27e061ae42ae153478b67034825e7a1e9ff0ed03
Pratyush Birole <birole.pratyush@gmail.com>
2023-11-07 21:57:32 +0530

    Add files via upload

---
+++ Strings/TolowerCase.java
@@ -0,0 +1,9 @@
+public class TolowerCase{
+    public static void main(String[] args) {
+        
+    }class Solution {
+        public String toLowerCase(String s) {
+             return s.toLowerCase();
+        }
+    }
+}
