8ac46f77447a715bcedad086af58c3cdab828a80
Pratyush Birole <birole.pratyush@gmail.com>
2023-11-08 21:30:33 +0530

    Add files via upload

---
+++ Strings/RemovePalindromicSubsequence.java
@@ -0,0 +1,15 @@
+public class RemovePalindromicSubsequence{
+    public static void main(String[] args) {
+    
+    }class Solution {
+        public int removePalindromeSub(String s) {
+            int i = 0, j = s.length() - 1;
+    
+            while(i < j)
+                if(s.charAt(i++) != s.charAt(j--))
+                    return 2;
+    
+            return 1;
+        }
+    }
+}
