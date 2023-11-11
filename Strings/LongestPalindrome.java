2462ccb4fd7a1a030e4947e12d093ef9655c8b9d
Pratyush Birole <birole.pratyush@gmail.com>
2023-11-11 20:59:16 +0530

    Add files via upload

---
+++ Strings/LongestPalindrome.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestPalindrome{
+    public static void main(String[] args) {
+        
+    }class Solution {
+    public int longestPalindrome(String s) {
+        int oddCount = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            if (map.get(ch) % 2 == 1)
+                oddCount++;
+            else
+                oddCount--;
+        }
+        if (oddCount > 1)
+            return s.length() - oddCount + 1;
+        return s.length();
+    }
+}
+}
