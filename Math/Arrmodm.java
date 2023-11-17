c87f1f330a0b4e804bc2a2e0728601a7c0f2d239
Pratyush Birole <birole.pratyush@gmail.com>
2023-11-17 21:03:46 +0530

    Add files via upload

--- Math/Arrmodm.java
+++ Math/Arrmodm.java
@@ -8,7 +8,16 @@ public class Arrmodm {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        long num =0;
+        int p=1000000007;
+       
+    }public static void arrmodm (int arr [], int p){
+        long num=0;
         long mult=1;
+        for(int i=n-1;i>=0;i--){
+            num = (num % p + ((arr[i] % p)*(mult % p)) % p) % p;
+            mult = (mult * 10) % p;
+        }
+        System.out.println(num);
+
     }
 }
