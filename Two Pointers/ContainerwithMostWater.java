c9bf7006c7537d6d33685491163ff001b288cd18
Pratyush Birole <birole.pratyush@gmail.com>
2023-11-21 23:15:45 +0530

    Add files via upload

---
+++ Two Pointers/ContainerwithMostWater.java
@@ -0,0 +1,24 @@
+public class ContainerwithMostWater{
+    public static void main(String[] args) {
+        
+    }class Solution {
+        public int maxArea(int[] height) {
+            int left = 0;
+            int right = height.length - 1;
+            int maxArea = 0;
+    
+            while (left < right) {
+                int currentArea = Math.min(height[left], height[right]) * (right - left);
+                maxArea = Math.max(maxArea, currentArea);
+    
+                if (height[left] < height[right]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+    
+            return maxArea;
+        }
+    }
+}
