aa83d57be9b6ecc1b5cee8740d2869dda6831b58
Pratyush Birole <birole.pratyush@gmail.com>
2023-11-23 23:53:40 +0530

    Add files via upload

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,27 +1,29 @@
 import java.util.*;
+
+
 public class InsertionSort {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-    
-        int arr []= new int[n];
-
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-insertionSort(arr);
-
-    }public static void insertionSort(int arr []){
-        int n=arr.length;
-        for(int j=n-2;j>=0;j--){
-            if(arr[j]>arr[j++]){
-                int temp=arr[j];
-                arr[j+1]=arr[j];
-                arr[j+1]=temp;
-            }else{
-                break;
-            }
-
-        }
+ public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int n=sc.nextInt();
+   int arr [] = new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
     }
+
+insertionsort(arr);
+
+ }public static void insertionsort(int arr[]){
+int n = arr.length;
+      for(int i=0;i<n;i++){
+for(int j=i-1;j>=0;j--){
+    if(arr[j]>arr[j+1]){
+        int temp =arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]= temp;
+    }else{
+        break;
+    }
+}
+}
+}
 }
