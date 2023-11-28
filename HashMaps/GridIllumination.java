01104d8fe2727552c56db60d73d5de12213db039
Pratyush Birole <birole.pratyush@gmail.com>
2023-11-28 23:48:48 +0530

    Add files via upload

--- HashMaps/GridIllumination.java
+++ HashMaps/GridIllumination.java
@@ -1,72 +1,28 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
-public class GridIllumination{
+public class GridIllumination {
     public static void main(String[] args) {
-        
-    }class Solution {
-    private int n;
-    private HashMap<Integer, Integer> rowIllum;
-    private HashMap<Integer, Integer> colIllum;
-    private HashMap<Integer, Integer> dia1Illum;
-    private HashMap<Integer, Integer> dia2Illum;
-    private HashMap<Integer, ArrayList<Integer>> lamps;
 
-    public boolean isIlluminated(int x, int y) {
-        return rowIllum.getOrDefault(x, 0) > 0 || colIllum.getOrDefault(y, 0) > 0 || 
-                dia1Illum.getOrDefault(x - y, 0) > 0 || dia2Illum.getOrDefault(x + y, 0) > 0;
-    }
-    public void turnOff(int x, int y) {
-        for(int i = Math.max(x - 1, 0); i < Math.min(x + 2, n); i++) {
-            if(lamps.containsKey(i)) {
-                ArrayList<Integer> l = lamps.get(i);
-                for(int j = Math.max(y - 1, 0); j < Math.min(y + 2, n); j++) {
-                    if(l.contains(j)) {
-                        rowIllum.put(i, rowIllum.get(i) - 1);
-                        colIllum.put(j, colIllum.get(j) - 1);
-                        dia1Illum.put(i - j, dia1Illum.get(i - j) - 1);
-                        dia2Illum.put(i + j, dia2Illum.get(i + j) - 1);
-                        l.remove((Integer)j);
-                    }
-                    if(l.isEmpty()) {
-                        lamps.remove(i);
-                    }
-                }
-            }
-        }
-    }
-    public void turnOn(int x, int y) {
-        if(!lamps.containsKey(x) || !lamps.get(x).contains(y)) {
-            lamps.computeIfAbsent(x, k -> {
-                return new ArrayList<Integer>();
-            }).add(y);
-            rowIllum.put(x, rowIllum.getOrDefault(x, 0) + 1);
-            colIllum.put(y, colIllum.getOrDefault(y, 0) + 1);
-            dia1Illum.put(x - y, dia1Illum.getOrDefault(x - y, 0) + 1);
-            dia2Illum.put(x + y, dia2Illum.getOrDefault(x + y, 0) + 1);
-        }
-    }
-    public int[] gridIllumination(int n, int[][] lamps, int[][] queries) {
-        this.n = n;
-        rowIllum = new HashMap<Integer, Integer>();
-        colIllum = new HashMap<Integer, Integer>();
-        dia1Illum = new HashMap<Integer, Integer>();
-        dia2Illum = new HashMap<Integer, Integer>();
-        this.lamps = new HashMap<Integer, ArrayList<Integer>>();
+    }public class int [] gridIllumination(int n, int [][] lamps, int [][] queries){
+        int n=lamps.length;ß
+        HashMap <Integer, Integer> m1, m2, m3,m4, m5;
+        m1= new HashMap<>();
+        m2= new HashMap<>();
+        m3= new HashMap<>();
+        m4= new HashMap<>();
+        m5= new HashMap<>();
 
-        for(int[] l : lamps) {
-            turnOn(l[0], l[1]);
+        for(int i=0;i<n;i++){
+            int x= lamps[i][0];
+            int y=lamps[i][1];
+
+            m1.put(x , m1.getOrDefault(x,0) + 1);
+            m2.put(y, m2.getorDefault(x,0)+1);3
+
         }
-        int[] ans = new int[queries.length];
-        int index = 0;
-        for(int[] q : queries) {
-            if(isIlluminated(q[0], q[1])) {
-                ans[index] = 1;
-                turnOff(q[0], q[1]);
-            }
-            index++;
-        }
-        return ans;
+
+
+
+
     }
-}
 }
---
+++ HashMaps/SumofSubarrayzeroandone.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class SumofSubarrayzeroandone{
+    public static void main(String[] args) {
+        
+    }public static int Subarraywithzeroandones(int arr []){
+        HashMap <Integer,Integer>map= new HashMap <>();
+        int ans=0;
+        for(int j=0;j<n;j++){
+            int 
+        }
+
+    }
+}
