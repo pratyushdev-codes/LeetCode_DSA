c704b8a682195eb1306aae9a6c41466c7cdfd5d2
Pratyush Birole <birole.pratyush@gmail.com>
2024-01-04 10:01:47 +0530

    Add files via upload

---
+++ Binary Search/Searchin2DmatrixII.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Searchin2DmatrixII {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] matrix = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int target = sc.nextInt();
+
+        Searchin2DmatrixII searchMatrixInstance = new Searchin2DmatrixII();
+        System.out.println(searchMatrixInstance.searchMatrix(matrix, target));
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
