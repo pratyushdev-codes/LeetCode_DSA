438a0fa91bdd99d6e5163cb5ed144ce61be7a877
Pratyush Birole <birole.pratyush@gmail.com>
2024-01-31 01:47:57 +0530

    Add files via upload

--- Trees/ConstructofTree.java
+++ Trees/ConstructofTree.java
@@ -1,5 +1,35 @@
+import javax.swing.tree.TreeNode;
+
 public class ConstructofTree{
     public static void main(String[] args) {
         
-    }
+    } 
+
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+             
+
+
+
+
+
+
+
+        }public TreeNode construct(int [] preorder, int ps, int pe, int [] inorder, int  is, int ie){
+            if(ps>pe ||is>ie) return null;
+
+        }
+        for(int i= is;i<ie;i++ ){
+            if(inorder[i]==preorder[ps]){
+                int idx= i;
+                break;
+        }
+
+        int count =idx-is; 
+
+
+        root.left=construct(preorder,ps+1,ps+count, inorder, is,  idx-1);
+        root.right= construct(preorder, ps+co, count, null, count, count))
+
+        
+        }
 }
