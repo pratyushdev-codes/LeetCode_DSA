f74db091ce62c2d14e2af0ef81aee98a08b9170d
Pratyush Birole <birole.pratyush@gmail.com>
2024-09-03 16:57:26 +0530

    Add files via upload

--- Graphs/BFS.java
+++ Graphs/BFS.java
@@ -1,58 +1,49 @@
 import java.util.*;
-
 public class BFS {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of nodes: ");
-        int n = sc.nextInt();  // number of nodes
-        System.out.print("Enter number of edges: ");
-        int m = sc.nextInt();  // number of edges
         
-        int[][] edges = new int[m][2];
 
+    //taking graph input here
+    Scanner sc= new Scanner(System.in);
+    int n = sc.nextInt();//nodes
+    int m = sc.nextInt();//edges
 
-        for (int i = 0; i < m; i++) {
-            edges[i][0] = sc.nextInt();
-            edges[i][1] = sc.nextInt();
-        }
-        sc.close();  
-        
-        BFS(n, m, edges);
+    int edges[][] = new int[m][2];
+
+    for(int i = 0 ; i < m ; i++){
+        edges[i][0]= sc.nextInt();
+        edges[i][1]= sc.nextInt();
+
     }
 
-    public static void BFS(int n, int m, int[][] edges) {
-        List<List<Integer>> graph = constructGraph(n, m, edges);
+}    public static void BFS(int [][] edges , int n , int m){
+        List<List<Integers>> graph = constructGraph( edges , n , m);
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
 
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] vis = new boolean[n];
 
-        q.add(0); // Start BFS from node 0 (or any other starting node)
-        vis[0] = true;
-
-        System.out.println("BFS traversal starting from node 0:");
-
-        while (!q.isEmpty()) {
-            int rem = q.remove();
-            System.out.print(rem + " ");  // Process the current node
+        queue.add(0);
+        visited[0] = true;
+        while(!queue.isEmpty()){
+            int rem =  queue.poll();
 
             List<Integer> nbrs = graph.get(rem);
-            for (int v : nbrs) {
-                if (!vis[v]) {
-                    q.add(v);
-                    vis[v] = true;
+            for(int v:nbrs){
+                if(!visited[v]){
+                    queue.add(v);
+                    visited[v]=true;
                 }
             }
         }
-    }
-
-    public static List<List<Integer>> constructGraph(int n, int m, int[][] edges) {
+    }  public static List<List<Integer>> constructGraph(int [][] edges , int n , int m){
         List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());  
+
+        for(int i = 0 ; i< n ; i++){
+            graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < m; i++) {
-            int u = edges[i][0];
+        for(int i = 0 ; i< m ; i++){
+            int u= edges[i][0];
             int v = edges[i][1];
             graph.get(u).add(v);
             graph.get(v).add(u);
@@ -60,4 +51,12 @@ public class BFS {
 
         return graph;
     }
+
+    
+
+
+
+
+
+
 }
